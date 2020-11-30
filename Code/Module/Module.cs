050eae850cae41747c683dcf898d196525fa73d8
aridai-shi <jakubkaczma5@gmail.com>
2020-11-30 17:45:04 +0100

    Added the whole template

---
+++ Code/Module/Module.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.Template
+{
+    public class Module : EverestModule {
+
+        // Only one alive module instance can exist at any given time.
+        public static Module Instance;
+
+        public Module() {
+            Instance = this;
+        }
+
+        // Set up any hooks, event handlers and your mod in general here.
+        // Load runs before Celeste itself has initialized properly.
+        public override void Load() {
+        }
+
+        // Optional, initialize anything after Celeste has initialized itself properly.
+        public override void Initialize() {
+        }
+
+        // Optional, do anything requiring either the Celeste or mod content here. 
+        // Usually involves Spritebanks or custom particle effects.
+        public override void LoadContent(bool firstLoad) {
+        }
+
+        // Unload the entirety of your mod's content. Free up any native resources.
+        public override void Unload() {
+        }
+
+    }
+}
+
